fffdc638fa65f87fe95443ef847d665909ad0ae7
Reggie Senal <reggie.senal@gmail.com>
2025-07-16 06:01:51 +0800

    add observables return type

--- http-observables/src/app/employee-service.ts
+++ http-observables/src/app/employee-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IEmployee } from './employee';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class EmployeeService {
   private _url: string = '/assets/data/employees.json';
   constructor(private http: HttpClient) {}
 
-  getEmployees() {
+  getEmployees(): Observable<IEmployee[]> {
     return this.http.get<IEmployee[]>(this._url);
   }
 }
