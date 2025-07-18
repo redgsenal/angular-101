17113124f545ea508486d2ec43e57c152bf93df2
Reggie Senal <reggie.senal@gmail.com>
2025-07-19 06:11:44 +0800

    routing

---
+++ routing-demo/src/app/department-list/department-list.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-list',
+  standalone: false,
+  template: `
+    <p>
+      department-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class DepartmentList {
+
+}
