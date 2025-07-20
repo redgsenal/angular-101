bb73b320c62c555a06696476db187c9d82bbfba3
Reggie <reggie.senal@gmail.com>
2025-07-21 06:10:36 +0800

    param observable

--- routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -9,15 +9,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DepartmentDetailComponent {
   public departmentId: string | null | undefined;
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     const id = this.route.snapshot.paramMap.get('id');
     console.log('Department ID:', id);
   }
 
   ngOnInit() {
     // You can also use this to fetch department details based on the ID
+    /* 
+    this method has a drawback: it only runs once when the component is initialized.
+    If the route changes while the component is still active, it won't update the departmentId.
+    
     const id = this.route.snapshot.paramMap.get('id');
-    console.log('Department ID on init:', id);
-    this.departmentId = id;
+    console.log('Department ID on init:', id); 
+    
+    */
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.departmentId = id;
+      // This will log the department ID whenever the route changes
+      console.log('Department ID from paramMap:', this.departmentId);
+    });
+  }
+
+  goNext() {
+    let nextId = parseInt(this.departmentId || '0', 10) + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
+  goPrevious() {
+    let previousId = parseInt(this.departmentId || '0', 10) - 1;
+    this.router.navigate(['/departments', previousId]);
   }
 }
